d21abbb022b0a2e381c1be361e1c35c234bcd9ad
Enayat Badru <Enayatbadru07@gmail.com>
2025-03-13 20:20:52 +0200

    `Updated dashboard.css and dashboard.js files with minor styling and functionality changes.`

--- app/assets/js/dashboard.js
+++ app/assets/js/dashboard.js
@@ -3,10 +3,13 @@ const btn = document.querySelector('.btn-mobile');
 const  menu = document.querySelector('#menu');
 const  body = document.querySelector('#body');
 
-btn.addEventListener('click', ()=>{
+btn.addEventListener('click', active)
+menu.addEventListener('click', active)
+
+function active(){
     btn.classList.toggle('active');
     menu.classList.toggle('active');
-})
+}
 
 window.addEventListener('scroll', ()=>{
     header.classList.toggle('sticky', window.scrollY > 0);
